143f6d291682b73c70ecdb3de2dcd1dedecaab4a
Thiago Garbazza <1093328+thiagogarbazza@users.noreply.github.com>
2023-10-13 09:35:01 -0300

    feat: create navbar for user-area and other-systems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InterfaceModule } from './interface/interface.module';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 // Necessário para que o pt-BR seja o locale default
 registerLocaleData(ptBR, 'pt-BR');
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    InterfaceModule
+    InterfaceModule,
+    NgbDropdownModule,
   ],
-  providers: [
-    { provide: LOCALE_ID, useValue: 'pt-BR' }
-  ],
-  bootstrap: [AppComponent]
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
