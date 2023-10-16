4c868bf08517b6578e01fb606db4ac68f43762cf
Thiago Garbazza <1093328+thiagogarbazza@users.noreply.github.com>
2023-10-16 15:14:48 -0300

    feat: create a theme switcher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public isThemeLight = true;
   title = 'angular-tour-of-heroes';
+
+  ngOnInit(): void {
+    this.isThemeLight = true;
+  }
+
+  onThemeSwitchChange() {
+    this.isThemeLight = !this.isThemeLight;
+
+    document.body.setAttribute(
+      'data-theme',
+      this.isThemeLight ? 'light' : 'dark'
+    );
+  }
 }
